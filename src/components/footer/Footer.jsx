a0ff0e627a796672f3aaa367d0c0764b2d2f1673
Facundo Pimentel <pimentelfacundo22@gmail.com>
2025-08-11 22:25:57 -0300

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        
+    <footer className="flex justify-around items-center">
+      <div className="text-white p-5 w-[50%]">
+        <h1 className="text-[30px] mb-3">Informacion Adicional</h1>
+        <h2>pimentelfacundo22@gmail.com</h2>
+        <h2>Principal: +54 9 3813203241</h2>
+        <p>Copyright © 2025 Facundo Pimentel.</p>
+      </div>
+
+      <div className="flex justify-center text-center text-white text-[40px]">
+        <h1>eslogan personal a futuro para poner</h1>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
